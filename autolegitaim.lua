977ada9e83945e400253c2bd926dac7b0e855660
staTickahoot <159580423+staTickahoot@users.noreply.github.com>
2024-07-26 17:00:17 -0400

    Create autolegitaim.lua

---
+++ autolegitaim.lua
@@ -0,0 +1,9 @@
+local specificGameId = 286090429
+
+if game.PlaceId == specificGameId then
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/staTickahoot/caramel/main/legitaim.lua"))()
+  
+else
+    print("silasaha so pro :weary:")
+  
+end
