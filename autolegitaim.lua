b664a51487afe13fdb1fc9e8fd6932205d7d78f9
staTickahoot <159580423+staTickahoot@users.noreply.github.com>
2024-07-26 17:03:12 -0400

    Update autolegitaim.lua

--- autolegitaim.lua
+++ autolegitaim.lua
@@ -1,6 +1,7 @@
 local specificGameId = 286090429
 
 if game.PlaceId == specificGameId then
+      print("loading string")
       loadstring(game:HttpGet("https://raw.githubusercontent.com/staTickahoot/caramel/main/legitaim.lua"))()
   
 else
